6a57e854323ac6215edff7f108e4a1db3bb1262b
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-23 18:21:38 -0300

    Initial Commit

---
+++ AnimatingShapes/AnimatingShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingShapesApp.swift
+//  AnimatingShapes
+//
+//  Created by Christian Manzaraz on 23/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
