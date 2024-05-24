ea24aa909ae347d1bd3aa314f79f435355a033b1
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-24 14:16:31 -0300

    Section 4 Rollin' Rainbow Use scaling and animation delays to create a wave effect.

--- AnimatingShapes/ExplodingView.swift
+++ AnimatingShapes/ExplodingView.swift
@@ -21,8 +21,8 @@ struct ExplodingView: View {
                         .frame(width: 20, height: 20)
                         .foregroundStyle(.indigo)
                         .offset(
-                            x: isExploded ? (Double.random(in: -1...1) * 500) : 0,
-                            y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
+                            x: isExploded ? (Double.random(in: -1...1) * 300) : 0,
+                            y: isExploded ? (Double.random(in: -1...1) * 300) : 0)
                         .opacity(isExploded ? 0 : 1)
                         .animation(.easeInOut.speed(0.6), value: isExploded)
                         .padding()
